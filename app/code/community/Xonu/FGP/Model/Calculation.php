c6c1ec0d66f2bfea4b201aa4c4a944c6f846c574
Andreas von Studnitz <avs@integer-net.de>
2014-02-05 12:05:24 +0100

    Adjust to Magento coding standards

--- app/code/community/Xonu/FGP/Model/Calculation.php
+++ app/code/community/Xonu/FGP/Model/Calculation.php
@@ -8,35 +8,36 @@ class Xonu_FGP_Model_Calculation extends Mage_Tax_Model_Calculation
     protected $adminSession = false;
 
     // set origin to destination
-	public function getRateOriginRequest($store = null)
-	{
+    public function getRateOriginRequest($store = null)
+    {
         if (Mage::app()->getStore()->isAdmin() || Mage::getDesign()->getArea() == 'adminhtml') {
             $this->adminSession = true; // creating order in the backend
         }
 
         $session = $this->getSession();
-		if($session->hasQuote() || $this->adminSession) // getQuote() would lead to infinite loop here when switching currency
-		{
-			$quote = $session->getQuote();
-			if($quote->getIsActive() || $this->adminSession)
-			{
-                // use destination of the existing quote as origin if quote exists
-				$request = $this->getRateRequest(
-						$quote->getShippingAddress(),
-						$quote->getBillingAddress(),
-						$quote->getCustomerTaxClassId(),
-						$store
-				);
+        if ($session->hasQuote() || $this->adminSession) { // getQuote() would lead to infinite loop here when switching currency
 
-				return $request;
-			}
+            $quote = $session->getQuote();
+
+            if ($quote->getIsActive() || $this->adminSession) {
+                // use destination of the existing quote as origin if quote exists
+                $request = $this->getRateRequest(
+                    $quote->getShippingAddress(),
+                    $quote->getBillingAddress(),
+                    $quote->getCustomerTaxClassId(),
+                    $store
+                );
+
+                return $request;
+            }
+
             if (Mage::getSingleton('customer/session')->isLoggedIn()) {
 
                 $customer = Mage::getSingleton('customer/session')->getCustomer();
                 if (($billingAddress = $customer->getDefaultBillingAddress())
                     && ($shippingAddress = $customer->getDefaultShippingAddress())) {
 
-                    // use destination of the existing quote as origin if quote exists
+                    // use customer addresses as origin if customer is logged in
                     $request = $this->getRateRequest(
                         $shippingAddress,
                         $billingAddress,
@@ -49,155 +50,148 @@ class Xonu_FGP_Model_Calculation extends Mage_Tax_Model_Calculation
             }
 
             return $this->getDefaultDestination();
-		}
-		else // quote is not available when switching the currency
-		{
-			return $this->getDefaultDestination();
-		}
-	}
+        }
 
-	public function getSession()
+        // quote is not available when switching the currency
+
+        return $this->getDefaultDestination();
+
+    }
+
+    public function getSession()
     {
         if ($this->adminSession) {
             return Mage::getSingleton('adminhtml/session_quote'); // order creation in the backend
         } else {
-			return Mage::getSingleton('checkout/session'); // default order creation in the frontend
-		}
+            return Mage::getSingleton('checkout/session'); // default order creation in the frontend
+        }
     }
 
     // sometimes it is required to use shipping address from the quote instead of the default address
-	public function getRateRequest(
-			$shippingAddress = null,
-			$billingAddress = null,
-			$customerTaxClass = null,
-			$store = null)
-	{
-		if ($shippingAddress === false && $billingAddress === false && $customerTaxClass === false) {
-			return $this->getRateOriginRequest($store);
-		}
-		$address    = new Varien_Object();
-		$customer   = $this->getCustomer();
-		$basedOn    = Mage::getStoreConfig(Mage_Tax_Model_Config::CONFIG_XML_PATH_BASED_ON, $store);
+    public function getRateRequest(
+        $shippingAddress = null,
+        $billingAddress = null,
+        $customerTaxClass = null,
+        $store = null)
+    {
+        if ($shippingAddress === false && $billingAddress === false && $customerTaxClass === false) {
+            return $this->getRateOriginRequest($store);
+        }
+        $address    = new Varien_Object();
+        $customer   = $this->getCustomer();
+        $basedOn    = Mage::getStoreConfig(Mage_Tax_Model_Config::CONFIG_XML_PATH_BASED_ON, $store);
 
-		if (($shippingAddress === false && $basedOn == 'shipping')
-				|| ($billingAddress === false && $basedOn == 'billing')) {
-			$basedOn = 'default';
-		} else {
-			if ((($billingAddress === false || is_null($billingAddress) || !$billingAddress->getCountryId())
-					&& $basedOn == 'billing')
-					|| (($shippingAddress === false || is_null($shippingAddress) || !$shippingAddress->getCountryId())
-							&& $basedOn == 'shipping')
-			){
-				$session = Mage::getSingleton('checkout/session');
-				if ($customer) {
-					$defBilling = $customer->getDefaultBillingAddress();
-					$defShipping = $customer->getDefaultShippingAddress();
+        if (($shippingAddress === false && $basedOn == 'shipping')
+            || ($billingAddress === false && $basedOn == 'billing')) {
+            $basedOn = 'default';
+        } else {
+            if ((($billingAddress === false || is_null($billingAddress) || !$billingAddress->getCountryId())
+                    && $basedOn == 'billing')
+                || (($shippingAddress === false || is_null($shippingAddress) || !$shippingAddress->getCountryId())
+                    && $basedOn == 'shipping')
+            ) {
+                $session = Mage::getSingleton('checkout/session');
+                if ($customer) {
+                    $defBilling = $customer->getDefaultBillingAddress();
+                    $defShipping = $customer->getDefaultShippingAddress();
 
-					if ($basedOn == 'billing' && $defBilling && $defBilling->getCountryId()) {
-						$billingAddress = $defBilling;
-					} else if ($basedOn == 'shipping' && $defShipping && $defShipping->getCountryId()) {
-						$shippingAddress = $defShipping;
-					} else {
-						if($session->hasQuote() || $this->adminSession)
-						{
-							$quote = $session->getQuote();
-							$isActive = $quote->getIsActive();
-							if($isActive)
-							{
-								$shippingAddress = $quote->getShippingAddress();
-								$billingAddress = $quote->getBillingAddress();
-							}
-							else{
-								$basedOn = 'default';
-							}
-						}
-						else{
-							$basedOn = 'default';
-						}
-					}
-				} else {
+                    if ($basedOn == 'billing' && $defBilling && $defBilling->getCountryId()) {
+                        $billingAddress = $defBilling;
+                    } else if ($basedOn == 'shipping' && $defShipping && $defShipping->getCountryId()) {
+                        $shippingAddress = $defShipping;
+                    } else {
+                        if($session->hasQuote() || $this->adminSession) {
+                            $quote = $session->getQuote();
+                            $isActive = $quote->getIsActive();
+                            if($isActive) {
+                                $shippingAddress = $quote->getShippingAddress();
+                                $billingAddress = $quote->getBillingAddress();
+                            } else {
+                                $basedOn = 'default';
+                            }
+                        } else {
+                            $basedOn = 'default';
+                        }
+                    }
+                } else {
 
-					if($session->hasQuote() || $this->adminSession)
-					{
-						$quote = $session->getQuote();
-						$isActive = $quote->getIsActive();
-						if($isActive)
-						{
-							$shippingAddress = $quote->getShippingAddress();
-							$billingAddress = $quote->getBillingAddress();
-						}
-						else {
-							$basedOn = 'default';
-						}
-					}
-					else {
-						$basedOn = 'default';
-					}
-				}
-			}
-		}
-		switch ($basedOn) {
-			case 'billing':
-				$address = $billingAddress;
-				break;
-			case 'shipping':
-				$address = $shippingAddress;
-				break;
-			case 'origin':
-				$address = $this->getRateOriginRequest($store);
-				break;
-			case 'default':
-				$address
-					->setCountryId(Mage::getStoreConfig(Mage_Tax_Model_Config::CONFIG_XML_PATH_DEFAULT_COUNTRY, $store))
-					->setRegionId(Mage::getStoreConfig(Mage_Tax_Model_Config::CONFIG_XML_PATH_DEFAULT_REGION, $store))
-					->setPostcode(Mage::getStoreConfig(Mage_Tax_Model_Config::CONFIG_XML_PATH_DEFAULT_POSTCODE, $store));
-				break;
-		}
+                    if($session->hasQuote() || $this->adminSession) {
+                        $quote = $session->getQuote();
+                        $isActive = $quote->getIsActive();
+                        if ($isActive) {
+                            $shippingAddress = $quote->getShippingAddress();
+                            $billingAddress = $quote->getBillingAddress();
+                        } else {
+                            $basedOn = 'default';
+                        }
+                    } else {
+                        $basedOn = 'default';
+                    }
+                }
+            }
+        }
+        switch ($basedOn) {
+            case 'billing':
+                $address = $billingAddress;
+                break;
+            case 'shipping':
+                $address = $shippingAddress;
+                break;
+            case 'origin':
+                $address = $this->getRateOriginRequest($store);
+                break;
+            case 'default':
+                $address
+                    ->setCountryId(Mage::getStoreConfig(Mage_Tax_Model_Config::CONFIG_XML_PATH_DEFAULT_COUNTRY, $store))
+                    ->setRegionId(Mage::getStoreConfig(Mage_Tax_Model_Config::CONFIG_XML_PATH_DEFAULT_REGION, $store))
+                    ->setPostcode(Mage::getStoreConfig(Mage_Tax_Model_Config::CONFIG_XML_PATH_DEFAULT_POSTCODE, $store));
+                break;
+        }
 
-		if (is_null($customerTaxClass) && $customer) {
-			$customerTaxClass = $customer->getTaxClassId();
-		} elseif (($customerTaxClass === false) || !$customer) {
-			$customerTaxClass = $this->getDefaultCustomerTaxClass($store);
-		}
+        if (is_null($customerTaxClass) && $customer) {
+            $customerTaxClass = $customer->getTaxClassId();
+        } elseif (($customerTaxClass === false) || !$customer) {
+            $customerTaxClass = $this->getDefaultCustomerTaxClass($store);
+        }
 
-		$request = new Varien_Object();
-		$request
-			->setCountryId($address->getCountryId())
-			->setRegionId($address->getRegionId())
-			->setPostcode($address->getPostcode())
-			->setStore($store)
-			->setCustomerClassId($customerTaxClass);
-		return $request;
-	}
+        $request = new Varien_Object();
+        $request
+            ->setCountryId($address->getCountryId())
+            ->setRegionId($address->getRegionId())
+            ->setPostcode($address->getPostcode())
+            ->setStore($store)
+            ->setCustomerClassId($customerTaxClass);
+        return $request;
+    }
 
 
-	private function getDefaultDestination($store = null)
-	{
+    protected function getDefaultDestination($store = null)
+    {
         $address = new Varien_Object();
-		$request = new Varien_Object();
+        $request = new Varien_Object();
 
-		$address
-			->setCountryId(Mage::getStoreConfig(Mage_Tax_Model_Config::CONFIG_XML_PATH_DEFAULT_COUNTRY, $store))
-			->setRegionId(Mage::getStoreConfig(Mage_Tax_Model_Config::CONFIG_XML_PATH_DEFAULT_REGION, $store))
-			->setPostcode(Mage::getStoreConfig(Mage_Tax_Model_Config::CONFIG_XML_PATH_DEFAULT_POSTCODE, $store));
+        $address
+            ->setCountryId(Mage::getStoreConfig(Mage_Tax_Model_Config::CONFIG_XML_PATH_DEFAULT_COUNTRY, $store))
+            ->setRegionId(Mage::getStoreConfig(Mage_Tax_Model_Config::CONFIG_XML_PATH_DEFAULT_REGION, $store))
+            ->setPostcode(Mage::getStoreConfig(Mage_Tax_Model_Config::CONFIG_XML_PATH_DEFAULT_POSTCODE, $store));
 
-		$customerTaxClass = null;
-		$customer = $this->getCustomer();
+        $customerTaxClass = null;
+        $customer = $this->getCustomer();
 
-		if (is_null($customerTaxClass) && $customer) {
-			$customerTaxClass = $customer->getTaxClassId();
-		} elseif (($customerTaxClass === false) || !$customer) {
-			$customerTaxClass = $this->getDefaultCustomerTaxClass($store);
-		}
+        if (is_null($customerTaxClass) && $customer) {
+            $customerTaxClass = $customer->getTaxClassId();
+        } elseif (($customerTaxClass === false) || !$customer) {
+            $customerTaxClass = $this->getDefaultCustomerTaxClass($store);
+        }
 
-		$request
-			->setCountryId($address->getCountryId())
-			->setRegionId($address->getRegionId())
-			->setPostcode($address->getPostcode())
-			->setStore($store)
-			->setCustomerClassId($customerTaxClass);
+        $request
+            ->setCountryId($address->getCountryId())
+            ->setRegionId($address->getRegionId())
+            ->setPostcode($address->getPostcode())
+            ->setStore($store)
+            ->setCustomerClassId($customerTaxClass);
 
 
-		return $request;
-	}
+        return $request;
+    }
 }
